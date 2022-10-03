2668ab774f42fa070af6f50b25eba8fdb448e834
raent25 <raent25@gmail.com>
2022-10-03 10:23:07 +0300

    animation header title and block with skills

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.html';
 import './index.scss';
-import { mult, sum } from './modules/calc';
-
-console.log(mult(3, 4));
-console.log(sum(3, 4));
+import scrollAnimation from './modules/calc';
+window.addEventListener("DOMContentLoaded", () => {
+    scrollAnimation();
+});
--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,2 +1,24 @@
-export const mult = (a, b) => a * b;
-export const sum = async(a, b) => a + b;
+function scrollAnimation(){
+    let windowScreen = window.screen.height,
+    windowScroll = document.documentElement.scrollTop;
+document.addEventListener('scroll',()=>{
+    
+    document.addEventListener('scroll', (e)=>{
+        if(windowScroll < document.documentElement.scrollTop && document.documentElement.scrollTop < windowScreen && !document.querySelector('.header').classList.contains('active')){
+            document.querySelector('.header').classList.add('active');
+            document.querySelector('.nav').classList.add('active');
+            document.querySelector('.header-title').classList.add('active');
+        }
+        else if(document.querySelector('.header').classList.contains('active') && document.documentElement.scrollTop == 0 && windowScroll > document.documentElement.scrollTop){
+            document.querySelector('.header').classList.remove('active');
+            document.querySelector('.nav').classList.remove('active');
+            document.querySelector('.header-title').classList.remove('active');
+        }
+        windowScroll = document.documentElement.scrollTop;
+    });
+});
+}
+
+
+
+export default scrollAnimation();
